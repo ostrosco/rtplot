79d1e336fd766d2d53cceda4f0e1a7e3af55f171
ostrosco <ostrosco@fastmail.fm>
2019-03-24 16:04:33 -0600

    Moving drawing functions into a library
    - Made a Figure library for initializing the window and drawing updates
      into the window.
    - Updated main to use the new library.

---
+++ src/figure.rs
@@ -0,0 +1,126 @@
+use glium::glutin::dpi::LogicalSize;
+use glium::implement_vertex;
+use glium::Surface;
+
+pub static VERTEX_SHADER: &'static str = r#"
+    #version 140
+
+    in vec2 position;
+
+    void main() {
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+"#;
+
+pub static FRAGMENT_SHADER: &'static str = r#"
+    #version 140
+
+    out vec4 color;
+
+    void main() {
+        color = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+"#;
+
+#[derive(Copy, Clone)]
+pub struct Vertex {
+    position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
+
+impl Vertex {
+    fn new(x: f32, y: f32) -> Self {
+        Vertex { position: [x, y] }
+    }
+}
+
+pub struct Figure {
+    pub events_loop: glium::glutin::EventsLoop,
+    display: glium::Display,
+    program: glium::Program,
+}
+
+impl Figure {
+    pub fn new() -> Self {
+        let events_loop = glium::glutin::EventsLoop::new();
+        let context = glium::glutin::ContextBuilder::new().with_vsync(true);
+        let window = glium::glutin::WindowBuilder::new()
+            .with_dimensions(LogicalSize {
+                width: 400.0,
+                height: 400.0,
+            })
+            .with_decorations(true)
+            .with_title("Plot");
+
+        let display =
+            glium::Display::new(window, context, &events_loop).unwrap();
+        let program = glium::Program::from_source(
+            &display,
+            VERTEX_SHADER,
+            FRAGMENT_SHADER,
+            None,
+        )
+        .unwrap();
+
+        Figure {
+            events_loop,
+            display,
+            program,
+        }
+    }
+
+    /// Take an array of points and draw it to the screen.
+    pub fn plot(&mut self, points: Vec<(f32, f32)>) {
+        // Grab the min and max points of the data and normalize it to fix onto
+        // the screen.
+        let min_x = points
+            .iter()
+            .min_by(|x, y| x.0.partial_cmp(&y.0).unwrap())
+            .unwrap()
+            .0;
+        let max_x = points
+            .iter()
+            .max_by(|x, y| x.0.partial_cmp(&y.0).unwrap())
+            .unwrap()
+            .0;
+        let min_y = points
+            .iter()
+            .min_by(|x, y| x.1.partial_cmp(&y.1).unwrap())
+            .unwrap()
+            .1;
+        let max_y = points
+            .iter()
+            .max_by(|x, y| x.1.partial_cmp(&y.1).unwrap())
+            .unwrap()
+            .1;
+
+        let mut vertices = vec![];
+        for point in points {
+            let x = 2.0 * (point.0 - min_x) / (max_x - min_x) - 1.0;
+            let y = (point.1 - min_y) / (max_y - min_y) - 0.5;
+            vertices.push(Vertex::new(x, y));
+        }
+
+        // Initialize a new vertex buffer. We could eventually use a single
+        // buffer and update it as needed, but I need to know if the use case
+        // is such where we should expect the same size data each iteration.
+        let vertex_buffer =
+            glium::VertexBuffer::new(&self.display, &vertices).unwrap();
+        let indices =
+            glium::index::NoIndices(glium::index::PrimitiveType::Points);
+        let mut target = self.display.draw();
+        target.clear_color(0.8, 0.8, 0.8, 1.0);
+        target
+            .draw(
+                &vertex_buffer,
+                &indices,
+                &self.program,
+                &glium::uniforms::EmptyUniforms,
+                &Default::default(),
+            )
+            .unwrap();
+
+        target.finish().unwrap();
+    }
+}
