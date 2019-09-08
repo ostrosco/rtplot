351ad54a8da758fdd75d024dab6452fcc4bccc3a
ostrosco <ostrosco@fastmail.fm>
2019-09-08 09:46:44 -0600

    Adding a new method to handle the display and event loops
    - New method on Figure to run a closure that receives and plots data
      while handling events. This way users don't have to worry about
      forgetting to call handle_events and having their window not be able
      to close.
    - Examples updated to use Figure::display.

--- examples/noise.rs
+++ examples/noise.rs
@@ -3,7 +3,6 @@ use rtplot::{Figure, PlotType};
 use std::thread;
 
 fn main() {
-    let mut status = true;
     let handle = thread::spawn(move || {
         let normal = Normal::new(0.0, 1.0);
         let mut rng = rand::thread_rng();
@@ -14,19 +13,15 @@ fn main() {
             .ylabel("Amplitude")
             .plot_type(PlotType::Line)
             .color(0x80, 0x00, 0x80);
-        loop {
-            if !status {
-                break;
-            }
 
+        Figure::display(figure, |fig| {
             let v: Vec<f32> = normal
                 .sample_iter(&mut rng)
                 .take(10)
                 .map(|x| x as f32)
                 .collect();
-            status = figure.handle_events();
-            figure.plot_stream(&v);
-        }
+            fig.plot_stream(&v);
+        });
     });
 
     handle.join().unwrap();
